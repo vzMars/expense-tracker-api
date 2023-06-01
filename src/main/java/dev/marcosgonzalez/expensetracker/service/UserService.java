8c1833bf0f7cc5158ad67d9076380f0f9f7b775b
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-06-01 10:38:32 -0400

    UserService that implements UserDetailsService and uses the injected UserRepository to load users by their username

---
+++ src/main/java/dev/marcosgonzalez/expensetracker/service/UserService.java
@@ -0,0 +1,23 @@
+package dev.marcosgonzalez.expensetracker.service;
+
+import dev.marcosgonzalez.expensetracker.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsernameIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found."));
+    }
+}
